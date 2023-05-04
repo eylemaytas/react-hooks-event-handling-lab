dea0657290f33f15c15f6690eaf17a76f7b1a4fe
Eylem Aytas <eylem.aytas@hotmail.com>
2023-05-04 15:17:34 -0400

    Modifies

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,12 @@
 // Code Keypad Component Here
+import react from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function onChangee() {
+    console.log("Entering password...");
+  }
+
+  return <input type="password" onChange={onChangee} />;
 }
 
 export default Keypad;
